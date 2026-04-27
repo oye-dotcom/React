b335c890e24f5fa64980359c3060c8b1edbef69a
oye-dotcom <oye275695@gmail.com>
2026-04-27 16:53:36 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
-import StatsSection from './components/StatsSection';
-import BoldApproaches from './components/BoldApproaches';
-import SustainingChange from './components/SustainingChange';
 import LatestAtEGov from './components/LatestAtEGov';
-import Supporters from './components/Supporters';
 import Footer from './components/Footer';
 
 function App() {
